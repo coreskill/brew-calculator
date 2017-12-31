2be709a0bc26e525255ccd80b97ed8f08d1e65d0
Kate Miháliková <kate@katemihalikova.cz>
2017-12-31 11:17:11 +0100

    Calculate one field from other two

--- src/index.js
+++ src/index.js
@@ -1,29 +1,57 @@
 const beansInput = document.querySelector("#beans");
 const ratioInput = document.querySelector("#ratio");
-
 const waterInput = document.querySelector("#water");
+
 const coffeeInput = document.querySelector("#coffee");
 
-const calculateWater = (beans, ratio) => {
-    let water = beans * ratio;
-    let coffee = water * 0.9;
+const calculateBeans = (water, ratio) => ratio === 0 ? 0 : water / ratio;
+const calculateRatio = (water, beans) => beans === 0 ? 0 : water / beans;
+const calculateWater = (beans, ratio) => beans * ratio;
+const calculateCoffee = (water) => water * 0.9;
 
-    return {water, coffee};
+let lastChanges = [ratioInput, beansInput];
+
+let beans = 8;
+let ratio = 16;
+let water = calculateWater(beans, ratio);
+let coffee = calculateCoffee(water);
+
+const onInputChange = ({target}) => {
+    if (lastChanges[0] !== target) {
+        lastChanges = [target, lastChanges[0]];
+    }
+
+    if (lastChanges.every(input => input.validity.valid)) {
+
+        if (!lastChanges.includes(beansInput)) {
+            water = waterInput.valueAsNumber;
+            ratio = ratioInput.valueAsNumber;
+            beans = calculateBeans(water, ratio);
+        } else if (!lastChanges.includes(ratioInput)) {
+            water = waterInput.valueAsNumber;
+            beans = beansInput.valueAsNumber;
+            ratio = calculateRatio(water, beans);
+        } else {
+            beans = beansInput.valueAsNumber;
+            ratio = ratioInput.valueAsNumber;
+            water = calculateWater(beans, ratio);
+        }
+
+        coffee = calculateCoffee(water);
+
+        writeInputValues();
+    }
 };
 
-const onInputChange = () => {
-    if (beansInput.validity.valid) {
-        let {water, coffee} = calculateWater(beansInput.valueAsNumber, ratioInput.valueAsNumber);
-
-        waterInput.value = water;
-        coffeeInput.value = coffee;
-    } else {
-        waterInput.value = "";
-        coffeeInput.value = "";
-    }
+const writeInputValues = () => {
+    beansInput.value = beans;
+    ratioInput.value = ratio;
+    waterInput.value = water;
+    coffeeInput.value = coffee;
 };
 
 beansInput.addEventListener("input", onInputChange);
 ratioInput.addEventListener("input", onInputChange);
+waterInput.addEventListener("input", onInputChange);
 
-onInputChange();
+writeInputValues();
